24fc69a7066abb21cf9329aad7872be6d8461460
surya9901 <surya9901@gmail.com>
2021-11-30 20:04:05 +0530

    added heroku url

--- src/Components/settings.js
+++ src/Components/settings.js
@@ -1,6 +1,7 @@
 const getsettings = () => {
     return {
-        api: "http://localhost:5000"
+        // api: "http://localhost:5000"
+        api: "https://diary-manager.herokuapp.com"
     }
 }
 
